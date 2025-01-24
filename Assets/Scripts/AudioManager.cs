6c36c71694e01ec5921c73740a59cf62fd6d7f1b
LoganKemper <161519847+LoganKemper@users.noreply.github.com>
2025-01-24 14:43:45 -0500

    Added DOTween, AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixerGroup mixerGroup;
+    [SerializeField] private AudioSource audioSource;
+
+    private List<AudioSource> audioSourcePool;
+    private readonly int initialPoolSize = 10;
+
+    public static AudioManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            InitializeAudioSourcePool();
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    private void InitializeAudioSourcePool()
+    {
+        audioSourcePool = new List<AudioSource>();
+        for (int i = 0; i < initialPoolSize; i++)
+        {
+            audioSourcePool.Add(CreateNewAudioSource());
+        }
+    }
+
+    private AudioSource GetPooledAudioSource()
+    {
+        foreach (AudioSource source in audioSourcePool)
+        {
+            if (!source.isPlaying)
+            {
+                source.gameObject.SetActive(true);
+                return source;
+            }
+        }
+
+        // If all sources are playing, expand the pool
+        Debug.Log("Expanding audio source pool");
+        AudioSource newSource = CreateNewAudioSource();
+        audioSourcePool.Add(newSource);
+        return newSource;
+    }
+
+    private AudioSource CreateNewAudioSource()
+    {
+        GameObject tempGO = new("PooledAudioSource");
+        tempGO.transform.SetParent(transform);
+        AudioSource tempAudioSource = tempGO.AddComponent<AudioSource>();
+        tempAudioSource.playOnAwake = false;
+        tempAudioSource.outputAudioMixerGroup = mixerGroup;
+        tempGO.SetActive(false);
+        return tempAudioSource;
+    }
+
+    public void PlaySoundAtPosition(AudioClip soundClip, Transform transform, float volume = 1f, float pitch = 1f)
+    {
+        if (soundClip != null)
+        {
+            AudioSource tempAudioSource = GetPooledAudioSource();
+            tempAudioSource.transform.position = transform.position;
+            tempAudioSource.clip = soundClip;
+            tempAudioSource.volume = volume * (1 / pitch); // I think this normalizes the volume after pitch adjustment
+            tempAudioSource.pitch = pitch;
+            tempAudioSource.PlayOneShot(soundClip);
+        }
+    }
+}
