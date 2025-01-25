fd6cbad7551035ad0357680d84cecfc0b30579ad
LoganKemper <161519847+LoganKemper@users.noreply.github.com>
2025-01-25 15:38:21 -0500

    Updated monolithic audio

--- Assets/Scripts/MonolithicAudio.cs
+++ Assets/Scripts/MonolithicAudio.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MonolithicAudio : MonoBehaviour
 {
@@ -26,6 +27,11 @@ public class MonolithicAudio : MonoBehaviour
         {
             StartLoop();
         }
+    }
+
+    public void PlayAudioClip(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 
     public void PlayAudioClip(AudioClip clip, float volume = 1f)
